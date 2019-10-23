fde491c13a1da89a10450751dd5ebd7dcdb24cd2
Sunghoon Ahn <ahnsunghoon0215@gmail.com>
2019-10-23 01:02:14 -0400

    Route Files with Express Router

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ const app = express();
 // Connect Database
 connectDB();
 
-app.get('/', (req,res) => res.send('API Running'));
+app.get('/', (req, res) => res.send('API Running'));
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 const PORT = process.env.PORT || 5000;
 
