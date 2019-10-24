70428b647a85aa4b72b6f3a8f5a835d977bc1967
Sunghoon Ahn <ahnsunghoon0215@gmail.com>
2019-10-23 23:56:27 -0400

    Updated routes folder location and files

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ connectDB();
 app.get('/', (req, res) => res.send('API Running'));
 
 // Define Routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/users', require('./routes/api/users'));
 
 const PORT = process.env.PORT || 5000;
 
