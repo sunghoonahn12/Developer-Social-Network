6e5a6ca0bbd65d97addaa8febac9a89e5d9b0457
Sunghoon Ahn <ahnsunghoon0215@gmail.com>
2019-11-24 12:26:52 -0500

    Get all profiles & profile by user id

--- config/db.js
+++ config/db.js
@@ -1,20 +1,21 @@
-const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(db, {
-            useNewUrlParser: true,
-            useCreateIndex: true
-        });
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true
+    });
 
-        console.log('MongoDB Connected....');
-    } catch(err) {
-        console.error(err.message);
-        // Exit process with failure
-        process.exit(1);
-    }
+    console.log("MongoDB Connected....");
+  } catch (err) {
+    console.error(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
