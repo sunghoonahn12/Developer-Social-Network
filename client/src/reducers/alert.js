847df158951557359882d9fcb6dad1cc3c2cdcf4
Sunghoon Ahn <ahnsunghoon0215@gmail.com>
2019-12-25 16:32:27 -0500

    User Login

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -5,7 +5,7 @@ const initialState = [];
 export default function(state = initialState, action) {
   const { type, payload } = action;
 
-  switch (action.type) {
+  switch (type) {
     case SET_ALERT:
       return [...state, action.payload];
     case REMOVE_ALERT:
