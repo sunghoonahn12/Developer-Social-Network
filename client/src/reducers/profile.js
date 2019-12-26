c99f7043aa16a3d3b9e2c68df78a0d58ef705c84
Sunghoon Ahn <ahnsunghoon0215@gmail.com>
2019-12-25 21:57:44 -0500

    String on the dashboard

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,5 @@
-import { GET_PROFILE, PROFILE_ERROR } from "../actions/types";
+import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE } from "../actions/types";
+import { STATES } from "mongoose";
 
 const initialState = {
   profile: null,
@@ -22,6 +23,13 @@ export default function(state = initialState, action) {
       return {
         ...state,
         error: payload,
+        loading: false
+      };
+    case CLEAR_PROFILE:
+      return {
+        ...state,
+        profile: null,
+        repos: [],
         loading: false
       };
     default:
