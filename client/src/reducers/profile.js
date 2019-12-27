8dccea2721fb1dd3f4599029ac5076bdc2d142cb
Sunghoon Ahn <ahnsunghoon0215@gmail.com>
2019-12-27 13:11:24 -0500

    Finish Profile Actions & Reducer

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -2,7 +2,9 @@ import {
   GET_PROFILE,
   PROFILE_ERROR,
   CLEAR_PROFILE,
-  UPDATE_PROFILE
+  UPDATE_PROFILE,
+  GET_PROFILES,
+  GET_REPOS
 } from "../actions/types";
 import { STATES } from "mongoose";
 
@@ -25,6 +27,12 @@ export default function(state = initialState, action) {
         profile: payload,
         loading: false
       };
+    case GET_PROFILES:
+      return {
+        ...state,
+        profiles: payload,
+        loading: false
+      };
     case PROFILE_ERROR:
       return {
         ...state,
@@ -36,6 +44,12 @@ export default function(state = initialState, action) {
         ...state,
         profile: null,
         repos: [],
+        loading: false
+      };
+    case GET_REPOS:
+      return {
+        ...state,
+        repos: payload,
         loading: false
       };
     default:
